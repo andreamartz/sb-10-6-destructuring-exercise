7f691d9dfc4343eacbe515075ec143b386a3670e
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-06-15 08:56:15 -0500

    Complete Object Destructuring 1

---
+++ app.js
@@ -0,0 +1,11 @@
+// Destructuring Exercise
+// Object Destructuring 1
+// What does the following code return/print?
+
+let facts = { numPlanets: 8, yearNeptuneDiscovered: 1846 };
+let { numPlanets, yearNeptuneDiscovered } = facts;
+
+console.log(numPlanets); // ?
+// it returns 8
+console.log(yearNeptuneDiscovered); // ?
+// it returns 1846
