b8aa627b7965872699c841da723e09ae50d12106
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-06-15 09:18:59 -0500

    Complete Array Destructuring 3

--- app.js
+++ app.js
@@ -84,3 +84,14 @@ console.log(aFewOfMyFavoriteThings); // ?
 //  { "Bright copper kettles",
 //  "warm woolen mittens",
 //  "Brown paper packages tied up with strings"}
+
+// *****************************************************
+// Array Destructuring 3
+// *****************************************************
+// What does the following code return/print?
+
+let numbers = [10, 20, 30];
+[numbers[1], numbers[2]] = [numbers[2], numbers[1]];
+
+console.log(numbers); // ?
+// it returns [10, 30, 20]
