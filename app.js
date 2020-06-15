7db6ba07bb38d49699ee3ab42de7d4496268d302
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-06-15 09:04:21 -0500

    Complete Object Destructuring 2

--- app.js
+++ app.js
@@ -6,11 +6,11 @@
 // What does the following code return/print?
 
 let facts = { numPlanets: 8, yearNeptuneDiscovered: 1846 };
-let { numPlanets, yearNeptuneDiscovered } = facts;
+// let { numPlanets, yearNeptuneDiscovered } = facts;
 
-console.log(numPlanets); // ?
+// console.log(numPlanets); // ?
 // it returns 8
-console.log(yearNeptuneDiscovered); // ?
+// console.log(yearNeptuneDiscovered); // ?
 // it returns 1846
 
 // *****************************************************
