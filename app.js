f3fdf7d3e3b6e1df0263c5fb9ff45c75c564862d
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-06-15 09:51:39 -0500

    Complete ES2015 One-Line Array Swap...

--- app.js
+++ app.js
@@ -121,10 +121,15 @@ const obj2 = {
   },
 };
 
-// console.log(
-//   ({
-//     numbers: { a, b },
-//   } = obj2)
-// );
 const { a2, b2 } = obj2.numbers;
 console.log(a2, b2);
+
+// ES5 Array Swap
+// var arr = [1, 2];
+// var temp = arr[0];
+// arr[0] = arr[1];
+// arr[1] = temp;
+
+// ES2015 One-Line Array Swap with Destructuring
+let arr = [1, 2];
+[arr[0], arr[1]] = [arr[1], arr[0]];
